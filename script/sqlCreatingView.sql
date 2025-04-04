87b03ec74fbc3e82762725dcc8111b399bcb7a53
girald02 <44089608+girald02@users.noreply.github.com>
2025-04-04 21:39:18 +0800

    Add files via upload

---
+++ script/sqlCreatingView.sql
@@ -0,0 +1,88 @@
+-- CREATE VIEW CALENDAR
+CREATE VIEW gold.calendar
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Calendar',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+
+-- CREATE VIEW CUSTOMER
+CREATE VIEW gold.customer
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Customers',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+
+
+-- CREATE VIEW PRODUCT CATEGORIES
+CREATE VIEW gold.product_categories
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Product_Categories',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+    
+
+-- CREATE VIEW PRODUCT
+CREATE VIEW gold.product
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Products',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+
+-- CREATE VIEW RETURN
+CREATE VIEW gold.return
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Returns',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+
+-- CREATE VIEW SALES
+CREATE VIEW gold.sales
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Sales',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+-- CREATE VIEW TERRITORIES
+CREATE VIEW gold.territories
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/AdventureWorks_Territories',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
+
+-- CREATE VIEW PRODUCT SUB CATEGORIES
+CREATE VIEW gold.product_sub_category
+AS
+SELECT *
+FROM
+OPENROWSET (
+    BULK 'https://storagedatalakeproject1.blob.core.windows.net/silver/Product_Subcategories',
+    FORMAT = 'PARQUET'
+) AS Quer1
+
